08088e81be95dcf4ba2f7f50f4b15337332061d8
maziazi <mazis9651@gmail.com>
2025-01-23 03:08:38 +0700

    Initial Commit

---
+++ sleep2Earn-Watch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sleep2Earn-Watch Watch App
+//
+//  Created by Muhamad Azis on 23/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sleep2Earn-Watch Watch App/sleep2Earn_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  sleep2Earn_WatchApp.swift
+//  sleep2Earn-Watch Watch App
+//
+//  Created by Muhamad Azis on 23/01/25.
+//
+
+import SwiftUI
+
+@main
+struct sleep2Earn_Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
